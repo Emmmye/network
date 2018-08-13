c6316802ffa75445b00f45760172b98fd78222f2
wh <1575308742@qq.com>
2018-08-13 14:55:30 +0800

    delete

--- 0.1-version-http-server/lib/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wh/mysql-5.6.14/include
-# Build directory: /home/wh/mysql-5.6.14/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
